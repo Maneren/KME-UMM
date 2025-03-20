78811974a25c4e561e4f2c72fe029173c3c15de0
Maneren <maneren731@gmail.com>
2025-03-20 14:25:34 +0100

    add damping

--- src/joints/spring.cpp
+++ src/joints/spring.cpp
@@ -1,7 +1,8 @@
 #include "spring.hpp"
+#include <Vector3.hpp>
 #include <print>
 
-void SpringJoint::update(const float) {
+void SpringJoint::update(const float delta) {
   const auto offset_a = _body_a->transform_offset(_anchor_a);
   const auto offset_b = _body_b->transform_offset(_anchor_b);
 
@@ -10,12 +11,21 @@ void SpringJoint::update(const float) {
 
   const auto connection = position_b - position_a;
 
-  const auto distance = connection.Length();
-  const auto rest_distance = distance - relaxed_length;
+  const auto length = connection.Length();
+  const auto extra_length = length - relaxed_length;
 
-  const auto force_magnitude = rest_distance / relaxed_length * strength;
+  auto force_magnitude = extra_length / relaxed_length * strength;
 
-  const auto force_a = connection.Scale(force_magnitude / distance);
+  if (damping > 0.f && last_length >= 0.f) {
+    const auto length_delta = last_length - length;
+    const auto velocity_magnitude = length_delta / delta;
+
+    force_magnitude -= damping * velocity_magnitude;
+  }
+
+  last_length = length;
+
+  const auto force_a = connection.Scale(force_magnitude / length);
   const auto force_b = -force_a;
 
   _body_a->apply_force(force_a, offset_a);
--- src/joints/spring.hpp
+++ src/joints/spring.hpp
@@ -10,15 +10,17 @@ public:
       std::shared_ptr<Body> body_b,
       raylib::Color color,
       const float strength,
-      const float relaxed_length
+      const float relaxed_length,
+      const float damping = 1.0f
   )
       : Joint(body_a, body_b, color), strength(strength),
-        relaxed_length(relaxed_length) {}
+        relaxed_length(relaxed_length), damping(damping) {}
 
   void update(const float dt) override;
 
 protected:
   float strength;
   float relaxed_length;
-  float falloff = 0.5f;
+  float last_length = -1.f;
+  float damping = 1.0f;
 };
